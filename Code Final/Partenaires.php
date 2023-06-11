5bb6e63cd8786cd6128284544ac0ea6d1a792283
Tom.algmt <57500834+Tomalgmt@users.noreply.github.com>
2023-06-12 00:45:28 +0200

    Create Partenaires.php

---
+++ Code Final/Partenaires.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+    <meta charset="utf-8" http-equiv="content-type" content="text/html;charset=iso-8859-1"/>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <div class="banniere">
+        <img src="images/LOGOS_JEUNES_1.png" class="partenairelogo" width="300px" style="margin-left: 2%;">
+        <div class="bannieretextepartenaire">
+            <a>PARTENAIRES  </a>
+        </div>
+    </div>
+    <nav class="liens">
+        <a href="jeune.php" class="Jeunebutton">JEUNE</a>
+        <a href="référent.php" class="Référentbutton">REFERENT</a>
+        <a href="consultant.php" class="Consultantbutton">CONSULTANT</a>
+        <div>
+            <a class="Partenairebuttonpage">PARTENAIRE</a>
+        </div>
+    </nav>
+    <br>
+    </div>
+    <br>
+    <div class="Partenairetexte1">
+        <a>JEUNES6.4 est un dispositif issu de la <a style="color: rgb(0, 0, 138);">charte de l’engagement</a> pour la<br>jeunesse signée en 2013 par des partenaires institutionnels...</a>
+    </div>
+    <br>
+    <div>
+        <img src="images/partenaires.png" class="partenairepartenaire">
+    </div>
+    <div style="margin-left: 30%; margin-top: 1%;">
+        <a  class="partenairebottomtext">...qui ont décidé de mettre en commun leurs actions pour les jeunes
+        <br>des Pyrénées-Atlantiques.</a>
+    </div>
+</body>
+
+</html>
