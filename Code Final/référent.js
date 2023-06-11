60a5791d7ec1e7b774e648e8fe0f9e4bf7cac445
Tom.algmt <57500834+Tomalgmt@users.noreply.github.com>
2023-06-12 00:07:31 +0200

    Add files via upload

---
+++ Code Final/référent.js
@@ -0,0 +1,25 @@
+
+
+//fonction associer au onclick
+function limiterCasesCochees() {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]'); //on récupère le nombre de checkbox
+    var nombreCasesCochées = 0;//compteur
+
+    // Compter le nombre de cases cochées avec une boucle
+
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (checkboxes[i].checked) {
+        nombreCasesCochées++;//on incrémente le compteur
+      }
+    }
+
+    // Désactiver les cases non cochées si le nombre maximum est atteint
+
+    for (var i = 0; i < checkboxes.length; i++) {
+      if (!checkboxes[i].checked && nombreCasesCochées >= 4) {//si il y a 4 cases cochées on descative les autres avec .disabled
+        checkboxes[i].disabled = true;
+      } else {
+        checkboxes[i].disabled = false;
+      }
+    }
+  }
