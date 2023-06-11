847f4be4117b8e99e37641eaa1a03bb1081173db
Tom.algmt <57500834+Tomalgmt@users.noreply.github.com>
2023-06-11 23:59:00 +0200

    Delete page_authentification.js

--- Code Final/page_authentification.js
+++
@@ -1,9 +0,0 @@
-
-function afficherFormulaireInscription() {
-    let formulaire_inscription = document.getElementById("formulaire_inscription");
-    let formulaire_connexion = document.getElementById("Formulaire_Connexion");
-
-        
-        formulaire_connexion.style.display = "none";
-        formulaire_inscription.style.display = "block";
-    }
