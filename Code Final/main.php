dec4e270805ea725f43952e7725f977242a7f558
Tom.algmt <57500834+Tomalgmt@users.noreply.github.com>
2023-06-11 23:44:17 +0200

    Add files via upload

---
+++ Code Final/main.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+    <meta charset="utf-8" http-equiv="content-type" content="text/html;charset=iso-8859-1"/>
+</head>
+<link rel="stylesheet" href="main.css">
+
+<body>
+
+<div id="head_title">
+    <img id=img src="images/LOGOS_JEUNES_1.png" alt="" />
+    <div class="head_text">
+        <p>Pour faire de l'engagement une valeur</p>
+</div>
+</div>
+
+<div id="sub_title">
+    <a class="Jeune" href="page_authentification.php">JEUNE</a>
+    <a class="Référent" href="Référent.php">REFERENT</a>
+    <a class="Consultant" href="Consultant.php">CONSULTANT</a>
+    <a class="Partenaire" href="Partenaires.php">PARTENAIRES</a>
+</div><br><br>    
+
+<div class="textarea">
+    <div class="flex1">
+        <a class = "header">
+            De quoi s'agit-il ?
+        </a> <br>
+
+        <a class = "text">
+        D’une opportunité : celle qu’un engagement quel qu’il soit puisse être
+        considérer à sa juste valeur !<br>
+        Toute expérience est source d’enrichissement et doit d’être reconnu
+        largement.<br>
+        Elle révèle un potentiel, l’expression d’un savoir-être à concrétiser.
+        </a> <br>
+    </div>
+    <div class="flex2">
+        <a class = "header">
+            A qui s'adresse t-il ?
+        </a> <br> 
+    
+        <a class = "text">
+        A vous, jeunes entre 16 et 30 ans, qui vous êtes investis spontanément
+        dans une association ou dans tout type d’action formelle ou infor-
+        melle, et qui avez partagé de votre temps, de votre énergie, pour apporter
+        un soutien, une aide, une compétence.<br><br>
+
+        A vous, responsables de structures ou référents d’un jour, qui avez
+        croisé la route de ces jeunes et avez bénéficié même ponctuellement de
+        cette implication citoyenne !<br>
+        C’est l’occasion de vous engager à votre tour pour ces jeunes en confir-
+        mant leur richesse pour en avoir été un temps les témoins mais aussi les
+        bénéficiaires !
+        </a> <br>
+    </div>
+    <div class="flex3">
+        <a class="text">
+        A vous, employeurs, recruteurs en ressources humaines,
+        représentants d’organismes de formation, qui recevez ces jeunes,
+        pour un emploi, un stage, un cursus de qualification, pour qui le savoir-être
+        constitue le premier fondement de toute capacité humaine.
+        <br><br>
+        <b>Cet engagement est une ressource à valoriser au fil d'un
+        parcours en 3 étapes :</b>
+        </a>
+    </div>    
+</div> <br>
+
+<div class="boxes">
+    <div class="box1">
+        <div class="top_box_text"> 1ère étape<br> la valorisation </div> <br>
+        <div class="bottom_box_text"> Décrivez votre expérience et mettez en avant ce que vous en avez retiré. </div>
+    </div>
+    <div class="box2">
+        <div class="top_box_text"> 2ème étape<br> la confirmation </div> <br>
+        <div class="bottom_box_text"> Confirmez cette expérience et ce que vous avez pu constater au contact de ce jeune. </div>
+    </div>
+    <div class="box3">
+        <div class="top_box_text"> 3ème étape<br> la consultation </div> <br>
+        <div class="bottom_box_text"> Validez cet engagement en prenant compte de sa valeur.                  </div>
+    </div>
+</div><br>        
+
+<body>
